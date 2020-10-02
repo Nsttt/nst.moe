3b3b27425b435267e13d312ee494c7a399b64d6b
Nestor <elnestinatorHD@gmail.com>
2020-10-02 19:06:58 +0200

    Added images and new design for the page.
    TODO: Add URL to original sources.

--- public/app.js
+++ public/app.js
@@ -1,45 +1,35 @@
-const numRand = Math.floor(Math.random() * 11);
-
 const app = new Vue({
   el: "#app",
   data: {
     url: "",
     alias: "",
     error: "",
-    number: numRand,
+    images: [
+      // TODO: Add Urls to original source.
+      { "picture": "./images/1.png", "url": "" },
+      { "picture": "./images/2.png", "url": "" },
+      { "picture": "./images/3.png", "url": "" },
+      { "picture": "./images/4.png", "url": "" },
+      { "picture": "./images/5.png", "url": "" },
+      { "picture": "./images/6.png", "url": "" },
+      { "picture": "./images/7.png", "url": "" },
+      { "picture": "./images/8.png", "url": "" },
+      { "picture": "./images/9.png", "url": "" },
+      { "picture": "./images/10.png", "url": "" },
+      { "picture": "./images/11.png", "url": "" },
+      { "picture": "./images/12.png", "url": "" },
+      { "picture": "./images/13.png", "url": "" },
+      { "picture": "./images/14.png", "url": "" },
+      { "picture": "./images/15.png", "url": "" },
+      { "picture": "./images/16.png", "url": "" },
+      { "picture": "./images/17.png", "url": "" },
+    ],
+    selectedImg: null,
+    selectedUrl: null,
     formVisible: true,
     created: null,
   },
   methods: {
-    addSource() {
-      switch (numRand) {
-        default:
-          break;
-        case 1:
-          break;
-        case 2:
-          break;
-        case 3:
-          break;
-        case 4:
-          break;
-        case 5:
-          break;
-        case 6:
-          break;
-        case 7:
-          break;
-        case 8:
-          break;
-        case 9:
-          break;
-        case 10:
-          break;
-        case 11:
-          break;
-      }
-    },
-
     async createUrl() {
       this.error = "";
       const response = await fetch("/url", {
@@ -55,18 +45,24 @@ const app = new Vue({
       if (response.ok) {
         const result = await response.json();
         this.formVisible = false;
-        this.created = `https://nst.sh/${result.alias}`;
+        this.created = `https://nst.sh/${result.alias}`;  
       } else if (response.status === 429) {
-        this.error = "Too mamy requests, try again in 30 seconds.";
+        this.error = "Too many requests, try again in 30 seconds.";
       } else {
         const result = await response.json();
         this.error = result.message;
       }
+      console.log(selectedImg, selectedUrl)
+    },
+
+    getImage(imgs) {
+      return imgs[Math.floor(Math.random() * this.images.length)];
     },
   },
-});
 
-const getRandomImage = () => {
-  document.getElementById("img").srcset = "./images/" + numRand + ".png";
-};
-getRandomImage();
+  created(){
+    const random = this.getImage(this.images)
+    this.selectedImg = random.picture;
+    this.selectedUrl = random.url;
+  }
+});
